946f5c8099a2258d18bdc00f382139e48141358c
chu-park <sa55797202@gmail.com>
2022-10-08 09:07:07 +0900

    1
    1

--- Color_bot.py
+++ Color_bot.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 import os
 import time
-from bs4 import BeautifulSoup
+#from bs4 import BeautifulSoup
 #from selenium import webdriver
 #from selenium.webdriver.common.by import By
 #from selenium.webdriver.common.keys import Keys
