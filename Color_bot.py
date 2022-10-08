351a7c05c37ec47faa95d6f0ed452c89948447f0
chu-park <sa55797202@gmail.com>
2022-10-08 09:21:19 +0900

    4
    4

--- Color_bot.py
+++ Color_bot.py
@@ -38,8 +38,41 @@ async def 염색(ctx, *, txt):
   await ctx.channel.send(b)
   await ctx.channel.send(c)
 
-  #driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+  driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
-  
+  driver.get("https://harp.fantazm.net/dye")
+  time.sleep(2)
+
+  element_1 = driver.find_element(By.NAME, "searchDyeColor1")
+  element_2 = driver.find_element(By.NAME, "searchDyeColor2")
+  element_3 = driver.find_element(By.NAME, "searchDyeColor3")
+
+  driver.find_element(By.NAME, "searchDyeColor1").click()
+  element_1.send_keys(query_txt_1)
+  element_1.send_keys(Keys.TAB)
+  element_2.send_keys(query_txt_2)
+  element_2.send_keys(Keys.TAB)
+  element_3.send_keys(query_txt_3)
+
+  driver.find_element(By.ID, "searchAmpulBtn").click()
+  time.sleep(1)
+  driver.find_element(By.XPATH, '//*[@id="sResult"]/div[1]/div[1]/span/i').click()
+  time.sleep(1)
+
+  color_txt = driver.find_element(By.XPATH, '//*[@id="plusResult"]/div[1]/div[1]/div/div/span')
+
+  txt_xpath_1 = driver.find_element(By.XPATH, '//*[@id="plusResult"]/div[1]/div[2]/div/h6')
+  txt_xpath_2 = driver.find_element(By.XPATH, '//*[@id="plusResult"]/div[1]/div[2]/div/div')
+
+  temperature_xpath = driver.find_element(By.XPATH, '//*[@id="btothetop"]/div[3]/div[3]/div/h4')
+
+  color_ = color_txt.text
+  temperature = temperature_xpath.text
+  txt_1 = txt_xpath_1.text
+  txt_2 = txt_xpath_2.text
+
+  embed=discord.Embed(title=color_, description="", color=temperature)
+  embed.set_footer(text=txt_1 + ('\n') + txt_2)
+  await ctx.send(embed=embed)
 
 client.run(os.environ['token'])
