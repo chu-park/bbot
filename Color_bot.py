e5ba683564e86f465281b700b26668b9f9d1897f
chu-park <sa55797202@gmail.com>
2022-10-08 09:14:08 +0900

    2
    2

--- Color_bot.py
+++ Color_bot.py
@@ -4,8 +4,8 @@ import os
 import time
 from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
+#from selenium.webdriver.common.by import By
+#from selenium.webdriver.common.keys import Keys
 
 client = commands.Bot(command_prefix='/', intents=discord.Intents.all())
 
